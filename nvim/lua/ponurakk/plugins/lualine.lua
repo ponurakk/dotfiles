b7c3adb5d505a23fa8f5bd2088f16f0a6a7f3f87
ponurakk <p0unrakg@int.pl>
2024-07-13 20:30:51 +0200

    chore(nvim): Update dependencies

--- nvim/lua/ponurakk/plugins/lualine.lua
+++ nvim/lua/ponurakk/plugins/lualine.lua
@@ -150,6 +150,7 @@ return {
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       always_divide_middle = true,
+      globalstatus = true,
     },
     sections = {
       lualine_a = { M.mode },
